356459fb54b3573733cbe0e4a9b43c64d1a75fcf
Manmit Pal <manmitpal348@gmail.com>
2025-03-30 19:41:09 +0530

    connecting to the database successfull!

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import mergedResolvers from "./resolvers/index.js" 
 import mergedTypeDefs from "./typeDefs/index.js"
+import { connectDB } from './db/connectDB.js';
 
 
 
@@ -38,5 +39,7 @@ app.use(
 await new Promise((resolve) =>
   httpServer.listen({ port: 4000 }, resolve),
 );
+
+await connectDB();
  
 console.log(`🚀 Server ready at http://localhost:4000/`);
