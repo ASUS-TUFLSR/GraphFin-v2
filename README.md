60aef1e848b24f1111ad260d9aa11fb20d31ac99
Manmit Pal <manmitpal348@gmail.com>
2025-03-31 22:31:55 +0530

    Implementing passportJs for user auth

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 - Created userTypeDef and transactionTypeDef which is a Type definitions to define the shape of the data available in the GraphQL API. They specify the types of objects that can be queried and the relationships between them.
 - Created userResolver which is a Resolver functions that determine how to fetch the data associated with each field in the schema.
 - Created index.js file in resolvers folder which will merger user & transaction resolver with mergerResolvers fn()
+- Created mongoose model one for user and other one for transaction they'll hold Schema assigned to it
+- Created .env file to store MONGO_URI and initialized a cluster in mongoDB atlas
+- Connected the database with the application
+- Will now proceed for user AUTH with PASSPORTJS
 
 
 
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import http from 'http';
 import cors from "cors";
 import dotenv from 'dotenv';
+import passport from 'passport';
+import session from 'express-session';
+import connectMongo from 'connect-mongodb-session';
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { buildContext } from "graphql-passport";
 import mergedResolvers from "./resolvers/index.js" 
 import mergedTypeDefs from "./typeDefs/index.js"
 import { connectDB } from './db/connectDB.js';
@@ -16,6 +20,34 @@ dotenv.config(); // If don't call the .envfn() we won't be able to use env varia
 const app = express();
 const httpServer = http.createServer(app);
 
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri:process.env.MONGO_URI,
+  collection: "sessions",
+
+})
+
+store.on("error", (err) => {
+  console.log(err)
+}) // for debugging purpose
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // this option specifies whether to save the session to the store on every request. if we say true then we'll have multiple session for the same user
+    saveUninitialized: false,// this option specifies whether to save unintialized sessions
+    cookie:{
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days after it session will expire requiring to login again
+      httpOnly: true, // it will prevent Cross-site Scripting (XSS) attacks
+    },
+    store:store
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
@@ -26,12 +58,16 @@ await server.start();
 
 app.use(
   '/',
-  cors(),
+  cors({
+    origin: "http://localhost:4000",
+    credentials: true,
+  }),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({req}),
+    // context is basically object which is shared across all resolvers
+    context: async ({ req, res }) => buildContext({ req, res }),
   }),
 );
 
