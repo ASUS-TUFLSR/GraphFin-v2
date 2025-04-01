59fb3c1d255153539cbc829699f00fd7fb858dfe
Manmit Pal <manmitpal348@gmail.com>
2025-04-01 21:29:57 +0530

    created transactions and transaction query

--- backend/resolvers/transactionResolver.js
+++ backend/resolvers/transactionResolver.js
@@ -1,6 +1,35 @@
+import Transaction from "../models/transactionModel.js"
 
 const transactionResolver = {
-    Query:{},
+    Query:{
+        transactions: async (_, __, context) => {
+            try {
+                if(!context.getUser()){
+                    throw new Error("Unauthorized")
+                }
+
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ userId });
+                return transactions;
+            } catch (err) {
+                console.error("Error in getting transactions: ", err);
+                throw new Error("Error getting transactions")
+            }
+        },
+
+        transaction: async (_, { transactionId }) => {
+            try {
+                const transaction = await Transaction.findById(transactionId);
+                return transaction
+            } catch (err) {
+                console.error("Error in getting transaction: ", err);
+                throw new Error("Error getting transaction")
+            }
+        },
+
+        // add category statistics query
+    },
     Mutation:{}
 }
 
